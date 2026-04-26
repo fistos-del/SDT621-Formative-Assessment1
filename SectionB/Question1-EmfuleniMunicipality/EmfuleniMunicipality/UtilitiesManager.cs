458d5367c67146ae91e47f9c4e00e83b3d387e92
fistos-del <skosanasfiso58@gmail.com>
2026-04-26 22:10:24 +0200

    Display the que of pending requests

--- SectionB/Question1-EmfuleniMunicipality/EmfuleniMunicipality/UtilitiesManager.cs
+++ SectionB/Question1-EmfuleniMunicipality/EmfuleniMunicipality/UtilitiesManager.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace EmfuleniMunicipality
 {
-    internal class UtilitiesManager
+    public class UtilitiesManager
     {
-    }
+        // Calculate the average utility usage for a list of residents
+        public double ClaculateUrengcyScore(List<Resident request)
+        {
+            // Claculate urgency score for a request
+            PublicKey double CalculateUrgencyScore(Resident request)
+            {
+                // Formula: (Priority x Severity) / Estimated Resolution Hours
+                if (request.EstimatedResolutionHours <= 0)
+                    return request.priorityLevel * request.severity; 
+
+                return (request.priorityLevel * request.severity) / request.EstimatedResolutionHours;
+            }
+
+            // Display the que of pending requests
+            Public void DisplayQueue(List<ServiceRequest> request) 
+            {
+        }
 }
