a68cb0cf226f35252ac83e661ba263051535ccdd
fistos-del <skosanasfiso58@gmail.com>
2026-04-26 16:55:33 +0200

    Complete SDT621 Formative Assessment 1

--- SectionA/Question1-StudentMarks/ConsoleApp1/Program.cs
+++ SectionA/Question1-StudentMarks/ConsoleApp1/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StudentMarks
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of students:");
+            Console.WriteLine("       STUDENT MARK CALCULATOR      ");
+            Console.Wrteline("====================================");
+
+            // Step 1: Gte student name
+            Console.WriteLine("Enter the name of the student: ");
+            string studentName = Console.ReadLine();
+
+            // Step 2: Declare variables
+            int marks1, marks2, marks3;
+            string input;
+            bool isvalid;
+
+        // Step 3: Gte variables Mark 1
+        do
+        {
+            Console.WriteLine("Enter marks for subject 1: ");
+            input = Console.ReadLine();
+            isvalid = int.TryParse(input, out marks1);
+            if (!isvalid)
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+            }
+        } while (!isvalid);
+
+        // Step 4: Gte variables Mark 2
+        do
+        {
+            Console.WriteLine("Enter marks for subject 2: ");
+            input = Console.ReadLine();
+            isvalid = int.TryParse(input, out marks2);
+            if (!isvalid)
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+        } while (!isvalid);
+    
+        // Step 5: Get variables Mark 3
+        do
+        {
+            Console.WriteLine("Enter marks for subject 3: ");
+            input = Console.ReadLine();
+            isvalid = int.TryParse(input, out marks3);
+            if (!isvalid)
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+        } while (!isvalid);
+
+    }
+{ }
