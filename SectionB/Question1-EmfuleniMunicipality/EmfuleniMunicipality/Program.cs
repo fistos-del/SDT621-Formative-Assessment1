045b276ad74eaa4016a4311413b7eb942cdb6349
fistos-del <skosanasfiso58@gmail.com>
2026-04-27 15:19:29 +0200

    CaptureResidents

--- SectionB/Question1-EmfuleniMunicipality/EmfuleniMunicipality/Program.cs
+++ SectionB/Question1-EmfuleniMunicipality/EmfuleniMunicipality/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using.collections.Generic;
-using System.Text.RegularExpressions;
+﻿using.collections.Generic;
+using System;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Transactions;
+using System.Xml.Linq;
 System.Linq;
 
 namespace EmfuleniManicpality
@@ -39,6 +41,63 @@ namespace EmfuleniManicpality
             Console.WriteLine();
         }
 
+        static void CaptureResidents()
+        {
+            Console.WriteLine("Enter the number of residents to register: ");
+            int residentsCount;
+            while (!int.TryParse(Console.ReadLine(), out residentsCount) || residentsCount <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer for the number of residents: ");
+            }
 
+            for (int i = 0; i < residentsCount; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("==================================================");
+                Console.WriteLine($"     RESIDENT {i + 1} REGISTRATION     ");
+                Console.WriteLine("==================================================\n");
+                Console.WriteLine("Enter resident's full name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Address: ");
+                string address = Console.ReadLine();
+
+                Console.Write("Enter Account Number: ");
+                string accountNumber = Console.ReadLine();
+                Console.WriteLine(EnterpriseServicesInteropOption Monthly Utilyty Usage(kWh): ");
+                double usage;
+                while (!double.TryParse(Console.ReadLine(), out usage) || usage < 0)
+                {
+                    Console.Write("Invalid input! Enter a valid usage amount: ");
+                }
+
+                residents.Add(new Residents(name, address, account, usage));
+                Console.WriteLine($"\nResident '{name}' registered successfully!\n");
+            }
+        }
+
+        static void CaptoureServiceRequests()
+        {
+            Console.WriteLine();
+            Console.WriteLine("==================================================");
+            Console.WriteLine("     SERVICE REQUEST REGISTRATION    ");
+            Console.WriteLine("==================================================\n");
+
+            Console.Write("Enter the number of service requests to register: ");
+            int requestsCount;
+            while (!int.TryParse(Console.ReadLine(), out requestsCount) || requestsCount <= 0)
+            {
+                Console.Write("Invalid input. Please enter a positive integer for the number of service requests: ");
+            }
+
+            for (int i = 0; i < requestsCount; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("==================================================")
+                Console.WriteLine($"         SERVICE REQUEST {i + 1} OF {requestCount}        ");
+                Console.WriteLine(("==================================================");
+
+
+            }
     }
 }
