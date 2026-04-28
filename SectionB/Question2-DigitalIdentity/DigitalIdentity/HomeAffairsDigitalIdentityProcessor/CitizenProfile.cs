f72637005812cd77f6c7687ab2b412e28e1629b5
fistos-del <skosanasfiso58@gmail.com>
2026-04-28 07:39:37 +0200

    HomeAffairsDigitalIdentityProcessor

--- SectionB/Question2-DigitalIdentity/DigitalIdentity/HomeAffairsDigitalIdentityProcessor/CitizenProfile.cs
+++ SectionB/Question2-DigitalIdentity/DigitalIdentity/HomeAffairsDigitalIdentityProcessor/CitizenProfile.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace HomeAffairsDigitalIdentityProcessor
 {
-    internal class CitizenProfile
+    public class CitizenProfile
     {
-    }
-}
+        // Properties with private setters (encapsulation)
+        public string FullName { get; private set; }
+        public string IDNumber { get; private set; }
+        public int Age { get; private set; }
+        public string CitizenshipStatus { get; private set; }
+
